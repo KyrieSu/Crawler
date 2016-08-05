7e2867f8dbc27174f038968bf338f64f8a8da159
KyrieSu <watermelo0326@gmail.com>
2016-08-06 01:19:18 +0800

    send cookie via request

--- reqCookie.js
+++ reqCookie.js
@@ -1,7 +1,7 @@
 var request = require('request');
 
 request( { url : 'http://www.cjihrig.com/development/php/hello_cookies.php', method : 'GET',
-        header: { 'Cookie' : 'name=Stranger'} },function(err,res,body){
+        headers: { 'Cookie' : 'name=Stranger'} },function(err,res,body){
             if(err){
                 console.log(err);
             }else{
