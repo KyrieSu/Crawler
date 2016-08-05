98eaf9c384c972abe6debc6882f4e83b1fd20294
KyrieSu <watermelo0326@gmail.com>
2016-08-05 17:10:07 +0800

    find the information of the link on youtube

--- youtube/index.js
+++ youtube/index.js
@@ -1,3 +1,18 @@
 var readline = require('readline');
-
+var tube = require('./parse.js')
 // getinfo(err,arr)
+var rl = readline.createInterface({
+    input : process.stdin,
+    output: process.stdout
+});
+
+rl.question('> Enter a link on youtube : ',function(ans){
+    tube.getinfo(ans,function(err,info){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(info);
+        }
+    })
+    rl.close();
+});
--- youtube/parse.js
+++ youtube/parse.js
@@ -1,6 +1,21 @@
-var request = require('require');
+var request = require('request');
 var cheerio = require('cheerio');
 
-//exports.getinfo = function(url,callback){
-    
-//}
+exports.getinfo = function(url,callback){
+    request(url,function(err,res,body){
+        if(err || res.statusCode==404){
+            callback(err,'404 not founded ~');
+            return;
+        }else{
+            $ = cheerio.load(body);
+            var info = [];
+            var video_title = $('#eow-title').each(function(index,elem){
+                info.push({ title :  $(elem).text().split('\n')[1].trim() });
+            });
+            var video_count = $('.watch-view-count').each(function(index,elem){
+                info[index].watch = $(elem).text().substring(5);
+            })
+        }
+        callback(err,info);
+    })
+}
