91c2d4eca8390ddcf17743f4c74741028772666a
KyrieSu <watermelo0326@gmail.com>
2016-08-08 04:59:14 +0800

    finish commit.js

--- project/commit.js
+++ project/commit.js
@@ -1,17 +1,23 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var commit_count = 23;
+var commit_count = 24;
 
 
-//function getNewCommit(callback){
+function getNewCommit(callback){
     request('https://github.com/KyrieSu/Crawler',function(err,res,body){
         if(err){
             callback(err,'fail in getNewCommit');
             return;
         }
         $ = cheerio.load(body);
+        // convert string into int
         var nowCommit = parseInt($('.commits span').text()); //$(.num .text-emphasized)
-        console.log(nowCommit == commit_count);
+        if(nowCommit!=commit_count){
+            commit_count = nowCommit;
+            callback(err,true);
+            return;
+        }
+        callback(err,false);
     })
-//}
+}
