bee5d9ec785f6e043b11187022cf982dbeab670b
KyrieSu <watermelo0326@gmail.com>
2016-08-08 04:08:37 +0800

    fix detail

--- project/sendmail.js
+++ project/sendmail.js
@@ -1,42 +1,44 @@
 var nodemailer = require('nodemailer');
 var fs = require('fs')
-exports.sendmail = function(){
 
 
 function readfile(callback){
     var info = [];
     fs.readFile('maillist.json','utf-8',function(err,data){
         if(err){
-            throw err;
+            callback(err,info);
             return;
         }
         info = JSON.parse(data); // convert string to JSON
-        callback(info);
+        callback(err,info);
     })
 }
 
-function send(){
-    readfile(function(data){
-        var reciever = '';
-        for(var i=0;i<data.length;i++){
-            reciever += data[i].mail;
+exports.send = function (callback){
+    readfile(function(err,data){
+        if(err){
+            callback(err,'fail in maillist');
+            return;
         }
+        var arr = [];
+        for(var i=0;i<data.length;i++){
+            arr.push(data[i].mail);
+        }
+        var receiver = arr.join(); //convert array into string
         //create object using SMTP transport
         var transporter = nodemailer.createTransport('smtps://amo30827@gmail.com:m2i0c8h2a8el@smtp.gmail.com');
         var mailOptions = {
-            from : "watermelo0326@gmail.com" , //sender e-mail address
-            to : reciever , // receiver e-mail address
+            from : "amo30827@gmail.com" , //sender e-mail address
+            to : receiver , // receiver e-mail address
             subject : 'find new push',
             text : 'New commit in KyrieSu/Crawler',
             html : '<a href = "https://github.com/KyrieSu/Crawler"> New commit in KyrieSu/Crawler </a>'
         };
         transporter.sendMail(mailOptions,function(err,info){
             if(err){
-                console.log(err);
+                callback(err,'fail in sendMail');
             }
-            console.log(info.response)
+            callback(err,'success');
         });
     })
-}
-
 }
